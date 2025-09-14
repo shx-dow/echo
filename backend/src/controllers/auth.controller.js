229604cd2e293abdc4ad4f3c1eab3408cf64c091
shx-dow <83426772+shx-dow@users.noreply.github.com>
2025-09-14 20:38:41 +0530

    feat: 🔒 add profile update and authentication middleware

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import cloudinary from "../lib/cloudinary.js";
 
 
 export const signup = async (req, res) => {
@@ -90,3 +91,34 @@ export const logout = (req, res) => {
         res.status(500).json({message: "Internal Server error"});
     }
 };
+
+export const updateProfile = async (req, res) => {
+    try {
+        const {profilePic} = req.body;
+        const userId = req.user._id;
+
+        if (!profilePic) {
+            return res.status(400).json({message: "Profile picture is required"});
+        }
+
+        const uploadResponse = await cloudinary.uploader.upload(profilePic)
+        const updatedUser = await User.findByIdAndDelete(userId, {profilePic: uploadResponse.secure_url}, {new: true})
+
+        res.status(200).json(updatedUser);
+
+    }
+    catch (error) {
+        console.log("Error in updateProfile controller", error);
+        res.status(500).json({message: "Internal Server error"});        
+    }
+};
+
+export const checkAuth = (req, res) => {
+    try {
+        res.status(200).json(req.user);
+    } 
+    catch (error) {
+        console.log("Error in checkAuth controller", error.message);
+        res.status(500).json({message: "Internal Server error"});        
+    }
+}
