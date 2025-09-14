1744c7b09ee92ae712b3c6e7e7c71e13598a72fc
shx-dow <83426772+shx-dow@users.noreply.github.com>
2025-09-14 16:29:33 +0530

    chore: 🔧 user database setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,18 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+import { connectDB } from './lib/db.js';
+
 import authRoutes from './routes/auth.route.js';
 
 const app = express();
 
+dotenv.config();
+const PORT = process.env.PORT
+
 app.use('/api/auth', authRoutes)
 
-app.listen(5001, () => {
-    console.log('Server is running on port 5001');
+app.listen(PORT, () => {
+    console.log('Server is running on port PORT:' + PORT);
+    connectDB();
 });
